9353e40a326611a867e682f89257b52dcee30348
Yuri Neves <yurineves1934@gmail.com>
2023-11-05 17:10:12 -0300

    feat: create component header

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './style';
+
+export const Header = () => {
+  return (
+    <S.Header>
+      <S.Title>A API Rick e Morty</S.Title>
+    </S.Header>
+  );
+};
---
+++ frontend/src/components/Header/style.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Header = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+  height: 300px;
+  width: 100%;
+`;
+
+export const Title = styled.h1`
+  font-size: 100px;
+  background-color: #202329;
+  color: #ff9800;
+`;
